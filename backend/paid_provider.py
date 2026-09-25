e2fcd39c1d063974262dcc69f7f202a72bdba58f
Shamayam Zara Abedin <zaraabedin58@gmail.com>
2026-09-26 02:49:47 +0530

    Update paid_provider.py

--- backend/paid_provider.py
+++ backend/paid_provider.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from backend.network import get_network_config
+from network import get_network_config
 from x402.http import (
     FacilitatorConfig,
     HTTPFacilitatorClient,
